e61680207bf87b16560d25b95f276c0fcd97a218
emmajphillips <emmjphillips@gmail.com>
2020-05-29 11:53:46 +0100

    deployment

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
     console.log('Mongo is Connected!')
   })
 
+app.use(express.static(`${__dirname}/frontend/build`))
+
 app.use(bodyParser.json())
 
 app.use('/api', router)
+
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/frontend/build/index.html`))
 
 app.listen(port, () => console.log(`Up and running on port ${port}`))
